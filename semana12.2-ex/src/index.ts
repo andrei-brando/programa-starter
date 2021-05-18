fd4896a50743e63071035d297ff0557c3a125b23
andrei.brando <andrei.brando@growdev.com.br>
2021-05-18 20:16:59 -0300

    commit

--- semana12.2-ex/src/index.ts
+++ semana12.2-ex/src/index.ts
@@ -204,6 +204,159 @@ app.post('/adicionar-pessoa', (req: Request, res: Response) => {
    return res.send('Pessoa salva');
 });
 
+// #12
+function transformIntoAscII(word: string, qtdSoma: number) {
+   let newWord = '';
+}
+
+app.post('/adicionar-time', (req: Request, res: Response) => {
+   const { nome, ano, estado } = req.body;
+
+   if (!nome || !ano || !estado) {
+      return res.status(404).json({
+         sucess: false,
+         message: 'Todos os dados devem ser informados'
+      });
+   }
+
+   const arrayString: Array<string> = ano.split('');
+   let anoInNumber = arrayString.map((value) => parseInt(value)).reduce((a, b) => a + b);
+
+   let nomeAsc = transformIntoAscII(nome, anoInNumber);
+
+   return res.status(200).json({
+      sucess: true,
+      data: {
+         anoInNumber: anoInNumber,
+         nomeAsc: nomeAsc,
+      },
+      message: 'Passou'
+   });
+});
+
+// #13
+app.post('/adicionar-valores-calculo', (req: Request, res: Response) => {
+   const dados: Object = req.body;
+
+   let numeros: Array<number> = Object.values(dados);
+
+   numeros.forEach((value) => {
+      if (typeof value !== 'number') {
+         return res.status(400).json({
+            sucess: false,
+            message: 'Algum dos valores digitados não é um número'
+         });
+      }
+   });
+
+   let inteiro = numeros.filter((value) => {
+      return !(value.toString().includes('2') || value.toString().includes('4'));
+   }).reduce((a, b) => a + b);
+
+   let pares: number = 0;
+   let impares: number = 0;
+
+
+   for (let i = 0; i <= inteiro; i++) {
+      i % 2 === 0 ? pares++ : impares++;
+   }
+
+   return res.status(200).json({
+      sucess: true,
+      soma: `A soma é: ${inteiro}`,
+      pares: `Quantidade de pares: ${pares}`,
+      impares: `Quantidade de pares: ${impares}`
+   });
+});
+
+// #14
+let pontos: Array<any> = [];
+
+app.post('/cadastrar-milhas', (req: Request, res: Response) => {
+   const { usuarioId, milhas, data } = req.body;
+
+   if (!usuarioId || !milhas || !data || data.length < 10) {
+      return res.status(400).json({
+         sucess: false,
+         message: 'Todos os dados devem ser preenchidos'
+      });
+   }
+
+   var ano: string = data.substring(6).match(/\d+/gi)?.join('');
+
+   if (ano.length < 4) {
+      return res.status(400).json({
+         sucess: false,
+         message: 'Ano está incorreto'
+      });
+   }
+
+   if (parseInt(ano) != 2020) {
+      return res.status(200).json({
+         sucess: true,
+         message: 'Ano não considerado'
+      });
+   }
+
+   const index = pontos.findIndex((e) => e.usuarioId == usuarioId);
+
+   if (index < 0) {
+      pontos.push({
+         usuarioId,
+         milhas,
+         data
+      });
+
+      return res.status(200).json({
+         sucess: true,
+         message: 'Milhas adicionadas',
+      });
+   } else {
+      const novasMilhas = pontos[index].milhas + milhas;
+      const novoUser = {
+         usuarioId,
+         milhas: novasMilhas,
+         data,
+      }
+
+      pontos.splice(index, 1, novoUser);
+
+      if (novasMilhas % 120000 == 0) {
+         return res.status(200).json({
+            sucess: true,
+            data: novoUser,
+            message: 'Você possui pontos para trocar hehe',
+         });
+      }
+   }
+});
+
+// #15
+app.post('/cadastrar-tentativas', (req: Request, res: Response) => {
+   const { numeroTentativas, numeroAcertos } = req.body;
+
+   const apv: number = Number(((numeroAcertos * 100) / numeroTentativas).toFixed());
+
+   let message = '';
+
+   if (apv >= 0 && apv < 40) {
+      message = 'Você precisa melhorar.';
+   } else if (apv >= 40 && apv < 60) {
+      message = 'Muito bom, mas ainda pode ser melhor.';
+   } else if (apv >= 60 && apv < 90) {
+      message = 'Parabéns, seu aproveitamento é acima da média';
+   } else if (apv >= 90 && apv < 99) {
+      message = 'Parabéns, você está entre os melhores';
+   } else {
+      message = 'Parabéns, você é o MELHOR';
+   }
+
+   return res.status(200).json({
+      sucess: true,
+      aproveitamento: apv,
+      message
+   });
+});
 
 app.listen(8080, () => {
    console.log('Rodando na porta 8080');
