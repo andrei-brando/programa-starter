a247e8f645d53c26b1f2c7e3ec72f2a80f36dcbb
andrei.brando <andrei.brando@growdev.com.br>
2021-05-25 21:27:59 -0300

    commit

--- semana13.2-ex/src/index2.ts
+++ semana13.2-ex/src/index2.ts
@@ -3,46 +3,71 @@ function dobrarEm5Segundos(n: number) {
    return new Promise((resolve, reject) => {
       setTimeout(() => {
          resolve(n * 2);
-      }, 5000);    
+      }, 5000);
    })
 }
 
 // dobrarEm5Segundos(5).then(r => console.log(r)).catch((e) => console.log(e));
 
-// async function somaAsync(x:number): number {
-//    let a, b, c: number;
+// async function somaAsync(x: number): Promise<number> {
+//    let a = await dobrarEm5Segundos(10);
+//    let b = await dobrarEm5Segundos(20);
+//    let c = await dobrarEm5Segundos(30);
 
-//    return await 0;
-// }
+//    return a + b + c + x;
+//  }
 
-// // const varA = await somaAsync(5);
+// console.log(somaAsync(20));
+
 
 // #2
-async function delay() {
-   setTimeout(() => {}, 1000);
-}
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
 
 async function umPorSegundo() {
-   await delay();
-   console.log('1s');
-   await delay();
+   await delay()
+   console.log('1s')
+   await delay()
    console.log('2s');
-   await delay();
+   await delay()
    console.log('3s');
 }
 
 // umPorSegundo();
 
 import axios from 'axios';
-async function getUserFromGitHub(user:string) {
-   const response = await axios.get(`https://api.github.com/users/${user}`);
 
-   if (!response) {
+async function getUserFromGithub(user: string) {
+   try {
+      const response = await axios.get(`https://api.github.com/users/${user}`);
+      console.log(response.data);
+   } catch (error) {
       console.log('Usuário não existe');
    }
-
-   console.log(response.data);
 }
 
-// getUserFromGitHub('djunior97');
-getUserFromGitHub('djuniordsadas97');
+// getUserFromGithub('djunior97');
+// getUserFromGithub('djunioriqdivqv97');
+
+class Github {
+   static async getRepositories(repo: string) {
+      try {
+         const response = await axios.get(`https://api.github.com/repos/${repo}`);
+         console.log(response.data);
+      } catch (error) {
+         console.log('Repositório não existe');
+      }
+   }
+}
+// Github.getRepositories('marcelo-growdev/scrapbook-es6');
+// Github.getRepositories('marcelo-growdev/qdbqqbqwn');
+
+const buscaUsuario = async (usuario: string) => {
+   try {
+      const response = await axios.get(`https://api.github.com/users/${usuario}`);
+      console.log(response.data);
+   } catch (error) {
+      console.log('Usuário não existe');
+   }
+}
+
+buscaUsuario('marcelo-growdev');
