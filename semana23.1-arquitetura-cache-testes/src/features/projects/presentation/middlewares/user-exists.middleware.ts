4106012e57d8d40bf01aad11b5f7a22a43079986
andrei-brando <albertbrando@hotmail.com>
2021-08-10 21:22:02 -0300

    refatorado para criar user repository

--- semana23.1-arquitetura-cache-testes/src/features/projects/presentation/middlewares/user-exists.middleware.ts
+++ semana23.1-arquitetura-cache-testes/src/features/projects/presentation/middlewares/user-exists.middleware.ts
@@ -1,13 +1,14 @@
 import { badRequest, InvalidParamError, ok } from "../../../../core/presentation";
 import { HttpRequest, HttpResponse } from "../../../../core/presentation";
-import { UserEntity } from "../../../../core/infra";
 import { Project } from "../../domain/models";
+import { UserRepository } from "../../infra";
+
 
 export class UserExistsMiddleware {
   async handle(request: HttpRequest): Promise<HttpResponse> {
     const body: Project = request.body;
 
-    const user = await UserEntity.findOne(body.userUid);
+    const user = await new UserRepository().getOne(body.userUid);
 
     if (!user) return badRequest(new InvalidParamError('userUid'));
 
