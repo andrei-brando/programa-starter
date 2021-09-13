46129c3c63b763ddae162af44e8b00d3cc8c862a
andrei-brando <albertbrando@hotmail.com>
2021-09-13 19:54:21 -0300

    copia projeto

---
+++ semana29.1&2-redux/src/pages/About/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import News from '../../components/News';
+
+export default function About() {
+  return (
+    <>
+      <h1>About Page</h1>
+      <p>
+        Cras ultricies ligula sed magna dictum porta. Vivamus magna justo,
+        lacinia eget consectetur sed, convallis at tellus. Mauris blandit
+        aliquet elit, eget tincidunt nibh pulvinar a.
+      </p>
+      <News />
+    </>
+  );
+}
